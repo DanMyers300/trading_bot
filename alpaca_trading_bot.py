61d4cf0c80e40060edc3e36566e5c2de471ddef7
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-06-03 04:34:03 +0000

    more options for tickers

--- alpaca_trading_bot.py
+++ alpaca_trading_bot.py
@@ -1,7 +1,6 @@
 """
-A simple trading bot
+A simple trading bot 
 """
-
 import os
 import time
 from dotenv import load_dotenv
@@ -19,29 +18,40 @@ API_KEY = os.getenv('API_KEY')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 trading_client = TradingClient(API_KEY, SECRET_KEY, paper=True)
+data_client = StockHistoricalDataClient(API_KEY, SECRET_KEY)
 
-# def get_account_information():
-#     """Getting account information"""
-#     account = trading_client.get_account()
-#     for property_name, value in account:
-#         print(f"\"{property_name}\": {value}")
+SYMBOLS = ['AAPL', 'MSFT', 'GOOGL']  # List of symbols to trade
 
-# get_account_information()
-
-SYMBOL = 'TSLA'
-data_client = StockHistoricalDataClient('<Your API Key>', '<Your API Secret>')
-trading_client = TradingClient('<Your API Key>', '<Your API Secret>', paper=True)
-while True:
-    request_params = StockLatestQuoteRequest(symbol_or_symbols=SYMBOL)
-    latest_quote = data_client.get_stock_latest_quote(request_params)[SYMBOL].ask_price
+def check_trading_condition(symb):
+    """Check trading condition for a given symbol"""
+    request_params = StockLatestQuoteRequest(symbol_or_symbols=symb)
+    latest_quote = data_client.get_stock_latest_quote(request_params)[symb].ask_price
     if latest_quote < 300.0:
-        buy_order = trading_client.submit_order(
-                                                order_data=MarketOrderRequest(
-                                                    SYMBOL=SYMBOL,
-                                                    qty=1,
-                                                    side=OrderSide.BUY,
-                                                    time_in_force=TimeInForce.DAY)
-                                                )
+        return True  # Buy condition
     elif latest_quote > 320.0:
-        sell_order = trading_client.close_position(SYMBOL)
-    time.sleep(60)
+        return False  # Sell condition
+    return None  # Hold condition
+
+while True:
+    for symbol in SYMBOLS:
+        try:
+            trading_condition = check_trading_condition(symbol)
+            if trading_condition is True:
+                buy_order = trading_client.submit_order(
+                    order_data=MarketOrderRequest(
+                        SYMBOL=symbol,
+                        qty=1,
+                        side=OrderSide.BUY,
+                        time_in_force=TimeInForce.DAY
+                    )
+                )
+                print(f"Bought {symbol} at {buy_order.filled_avg_price}")
+            elif trading_condition is False:
+                sell_order = trading_client.close_position(symbol)
+                print(f"Sold {symbol} at {sell_order.filled_avg_price}")
+            else:
+                print(f"Holding {symbol}")
+        # pylint: disable=broad-except
+        except Exception as e:
+            print(f"Error occurred for {symbol}: {str(e)}")
+    time.sleep(60)  # Sleep for 60 seconds before checking again
