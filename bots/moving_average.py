29f8740d3148b6078ec206c747865830e220110a
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-06-07 10:49:27 +0000

    testing some bots

--- bots/moving_average.py
+++ bots/moving_average.py
@@ -1,81 +1,61 @@
 import os
-import time
-from datetime import datetime
-import numpy as np
 from dotenv import load_dotenv
-from alpaca_trade_api import REST
+
+from alpaca.trading.client import TradingClient
+from alpaca.data.historical import StockHistoricalDataClient
+from alpaca.data.requests import StockBarsRequest
+from alpaca.data.timeframe import TimeFrame, TimeFrameUnit
 
 load_dotenv()
 
-ALPACA_API_KEY = os.getenv("API_KEY")
-ALPACA_SECRET_KEY = os.getenv("SECRET_KEY")
-SYMBOLS = ["AAPL", "MSFT", "GOOGL"]  # List of symbols to trade
+API_KEY = os.getenv('API_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-api = REST(ALPACA_API_KEY, ALPACA_SECRET_KEY)
+trading_client = TradingClient(API_KEY, SECRET_KEY)
+data_client = StockHistoricalDataClient(API_KEY, SECRET_KEY)
 
-# Dictionary to keep track of traded symbols
-traded_symbols = {}
+SYMBOLS = ['AAPL', 'MSFT', 'GOOGL']  # List of symbols to trade
+MA_PERIOD = 50  # Number of periods for the moving average
 
+def calculate_moving_average(symbol):
+    """Calculate the moving average for a given symbol"""
+    request_params = StockBarsRequest(
+        symbol_or_symbols=symbol,
+        timeframe=TimeFrame(15, TimeFrameUnit.Minute)
+    )
+    historical_data = data_client.get_stock_bars(request_params)
+    close_prices = [bar.c for bar in historical_data[symbol]]
+    moving_average = sum(close_prices[-MA_PERIOD:]) / MA_PERIOD
+    return moving_average
 
 def check_trading_condition(symbol):
-    "Fetch stock data for the symbol"
-    stock_data = api.get_bars(symbol, "day", limit=100).df
-
-    # Calculate moving averages
-    stock_data["20_SMA"] = (
-        stock_data[symbol]["close"].rolling(window=20, min_periods=1).mean()
+    """Check trading condition for a given symbol based on moving averages"""
+    moving_average = calculate_moving_average(symbol)
+    request_params = StockBarsRequest(
+        symbol_or_symbols=symbol,
+        timeframe=TimeFrame(1, TimeFrameUnit.Day)
     )
-    stock_data["10_SMA"] = (
-        stock_data[symbol]["close"].rolling(window=10, min_periods=1).mean()
-    )
+    historical_data = data_client.get_stock_bars(request_params)
+    latest_bar = historical_data[symbol][-1]
+    current_price = latest_bar.c
+    if current_price > moving_average:
+        return True  # Buy
+    elif current_price < moving_average:
+        return False  # Sell
+    else:
+        return None  # Hold
 
-    # Check for crossover points
-    stock_data["Cross"] = np.where(
-        stock_data["10_SMA"] > stock_data["20_SMA"], 1.0, 0.0
-    )
-    stock_data["Signal"] = stock_data["Cross"].diff()
-
-    # Map numbers to words
-    map_dict = {-1.0: "sell", 1.0: "buy", 0.0: "none"}
-    stock_data["Signal"] = stock_data["Signal"].map(map_dict)
-
-    # Get the latest signal
-    latest_signal = stock_data.iloc[-1]["Signal"]
-
-    return latest_signal
-
-
-for symbol_loop in SYMBOLS:
-    if symbol_loop in traded_symbols:
-        print(f"Already traded {symbol_loop} today")
-        continue
-
+for symbol in SYMBOLS:
     try:
-        trading_condition = check_trading_condition(symbol_loop)
-        if trading_condition == "buy":
-            # Place a buy order
-            api.submit_order(
-                symbol=symbol_loop,
-                qty=1,
-                side="buy",
-                type="market",
-                time_in_force="gtc",
-            )
-            print(f"Bought {symbol_loop}")
-            traded_symbols[symbol_loop] = True
-        elif trading_condition == "sell":
-            # Place a sell order to close the position
-            api.submit_order(
-                symbol=symbol_loop,
-                qty=1,
-                side="sell",
-                type="market",
-                time_in_force="gtc",
-            )
-            print(f"Sold {symbol_loop}")
-            traded_symbols[symbol_loop] = True
+        trading_condition = check_trading_condition(symbol)
+        if trading_condition is True:
+            # Buy logic
+            print(f"Buy {symbol}")
+        elif trading_condition is False:
+            # Sell logic
+            print(f"Sell {symbol}")
         else:
-            print(f"Holding {symbol_loop}")
-    # pylint: disable=broad-except
+            # Hold logic
+            print(f"Holding {symbol}")
     except Exception as e:
-        print(f"Error occurred for {symbol_loop}: {str(e)}")
+        print(f"Error occurred for {symbol}: {str(e)}")
