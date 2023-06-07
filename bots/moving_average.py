8a5d89a658d51a7d4a2ad9ded6be244fe1025144
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-06-07 09:26:33 +0000

    new bot

--- bots/moving_average.py
+++ bots/moving_average.py
@@ -1,8 +1,6 @@
-"A moving average trading bot"
-
 import os
 import time
-from datetime import datetime, time as dt_time
+from datetime import datetime
 import numpy as np
 from dotenv import load_dotenv
 from alpaca_trade_api import REST
@@ -14,6 +12,9 @@ ALPACA_SECRET_KEY = os.getenv("SECRET_KEY")
 SYMBOLS = ["AAPL", "MSFT", "GOOGL"]  # List of symbols to trade
 
 api = REST(ALPACA_API_KEY, ALPACA_SECRET_KEY)
+
+# Dictionary to keep track of traded symbols
+traded_symbols = {}
 
 
 def check_trading_condition(symbol):
@@ -44,48 +45,37 @@ def check_trading_condition(symbol):
     return latest_signal
 
 
-def is_trading_hours():
-    "Check if current time is within trading hours"
-    now = datetime.now().time()
-    start_time = dt_time(9, 30)  # Trading starts at 9:30 AM
-    end_time = dt_time(16)  # Trading ends at 4:00 PM
-
-    return start_time <= now <= end_time
-
-
-while True:
-    if not is_trading_hours():
-        print("Outside trading hours")
-        time.sleep(60)  # Sleep for 60 seconds before checking again
+for symbol_loop in SYMBOLS:
+    if symbol_loop in traded_symbols:
+        print(f"Already traded {symbol_loop} today")
         continue
 
-    for symbol_loop in SYMBOLS:
-        try:
-            trading_condition = check_trading_condition(symbol_loop)
-            if trading_condition == "buy":
-                # Place a buy order
-                api.submit_order(
-                    symbol=symbol_loop,
-                    qty=1,
-                    side="buy",
-                    type="market",
-                    time_in_force="gtc",
-                )
-                print(f"Bought {symbol_loop}")
-            elif trading_condition == "sell":
-                # Place a sell order to close the position
-                api.submit_order(
-                    symbol=symbol_loop,
-                    qty=1,
-                    side="sell",
-                    type="market",
-                    time_in_force="gtc",
-                )
-                print(f"Sold {symbol_loop}")
-            else:
-                print(f"Holding {symbol_loop}")
-        # pylint: disable=broad-except
-        except Exception as e:
-            print(f"Error occurred for {symbol_loop}: {str(e)}")
-
-    time.sleep(60)  # Sleep for 60 seconds before checking again
+    try:
+        trading_condition = check_trading_condition(symbol_loop)
+        if trading_condition == "buy":
+            # Place a buy order
+            api.submit_order(
+                symbol=symbol_loop,
+                qty=1,
+                side="buy",
+                type="market",
+                time_in_force="gtc",
+            )
+            print(f"Bought {symbol_loop}")
+            traded_symbols[symbol_loop] = True
+        elif trading_condition == "sell":
+            # Place a sell order to close the position
+            api.submit_order(
+                symbol=symbol_loop,
+                qty=1,
+                side="sell",
+                type="market",
+                time_in_force="gtc",
+            )
+            print(f"Sold {symbol_loop}")
+            traded_symbols[symbol_loop] = True
+        else:
+            print(f"Holding {symbol_loop}")
+    # pylint: disable=broad-except
+    except Exception as e:
+        print(f"Error occurred for {symbol_loop}: {str(e)}")
